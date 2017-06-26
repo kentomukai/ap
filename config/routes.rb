64ac067d4d1c6507de3037372e79a77b66c34627
kentomukai <ken.0319@icloud.com>
2017-06-26 16:27:40 +0900

    CSS and inputBox

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   #メイン画面
   root 'static_pages#home'
-
+  
+  get    '/search',  to: 'connections#connect'
   get    '/help',    to: 'static_pages#help'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'

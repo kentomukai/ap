505bbebf4170bb0009d122bc593710e282f293c6
kentomukai <ken.0319@icloud.com>
2017-06-26 15:48:07 +0900

    0626_start

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      redirect_to root_path
     else
       # エラーメッセージを作成する
       flash.now[:danger] = '無効なメールアドレスとパスワードの組み合わせです。'
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   #メイン画面
   root 'static_pages#home'
 
-  get 'sessions/new'
   get    '/help',    to: 'static_pages#help'
-  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
 end

e6d1f2d55c039393930e3ac6be314593b4f16e54
kentomukai <ken.0319@icloud.com>
2017-06-27 13:19:12 +0900

    tiny

--- lib/searching.rb
+++ lib/searching.rb
@@ -1,23 +1,3 @@
 #検索結果の初期化
 require 'FileUtils'
-FileUtils.rm_rf('/Users/kentomukai/db/db_search/output')
-FileUtils.mkdir('/Users/kentomukai/db/db_search/output')
-
-#データベースへ接続
-require 'mysql'
-db = Mysql::init
-db.connect("127.0.0.1", "root", "ken0912e", "od")
-
-#データベースを検索
-statement = "call searching('労働');"
-db.query(statement)
-db.close
-
-#空のCSVファイルを削除
-Dir.chdir("/Users/kentomukai/db/db_search/output")
-nameArray = Dir.glob("*.csv")
-nameArray.each do |fileName|
-    if ! File.size?("/Users/kentomukai/db/db_search/output/#{fileName}")
-        File.delete "/Users/kentomukai/db/db_search/output/#{fileName}"
-    end
-end
+FileUtils.mkdir('/Users/kentomukai/ap/向井参上')
